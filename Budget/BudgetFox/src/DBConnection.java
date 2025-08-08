ce36703be5ca22b166e6be9510bbbf07ef9f380b
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-08-08 06:31:22 +0530

    Update DBConnection.java

--- Budget/BudgetFox/src/DBConnection.java
+++ Budget/BudgetFox/src/DBConnection.java
@@ -1,15 +1,17 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection;              //Connection is the interface used to manage a DB connection.
+import java.sql.DriverManager;           //DriverManager is used to get the connection to the database.
+import java.sql.SQLException;            // SQLException handles database-related errors.
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/budgetfox_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root"; // Update if needed
+    private static final String PASSWORD = "root"; 
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            //Attempts to establish a connection using DriverManager, 
+            //If successful, it returns the Connection object.
+            return DriverManager.getConnection(URL, USER, PASSWORD);    
         } catch (SQLException e) {
             System.out.println("❌ Failed to connect to database");
             e.printStackTrace();
@@ -17,3 +19,4 @@ public class DBConnection {
         }
     }
 }
+

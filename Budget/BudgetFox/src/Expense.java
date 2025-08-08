860de8f21c3d82d541141fe308715ab500136ece
Janjitha Sa <145108748+Janjitha@users.noreply.github.com>
2025-08-08 06:46:50 +0530

    Update Expense.java

--- Budget/BudgetFox/src/Expense.java
+++ Budget/BudgetFox/src/Expense.java
@@ -4,15 +4,25 @@ public class Expense {
     private String date;
     private String notes;
 
+    // This is a constructor used to initialize a new Expense object with specific values.
     public Expense(String category, double amount, String date, String notes) {
         this.category = category;
         this.amount = amount;
         this.date = date;
         this.notes = notes;
     }
-
-    public String getCategory() { return category; }
-    public double getAmount() { return amount; }
-    public String getDate() { return date; }
-    public String getNotes() { return notes; }
+    //There are no setter methods here because the object is treated as read-only once created
+    public String getCategory() { 
+        return category; 
+    }
+    public double getAmount() { 
+        return amount; 
+    }
+    public String getDate() { 
+        return date; 
+    }
+    public String getNotes() { 
+        return notes; 
+    }
 }
+
